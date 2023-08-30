ad73c49b88f12ca24968a897f6ac14d4846b5b38
harshmayan <harshmayani07@gmail.com>
2023-08-30 20:51:43 +0530

    moviecard make responsive

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -11,20 +11,19 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
   const { openModal } = useInfoModalStore();
   return (
-    <div className="group bg-zinc-900 col-span relative h-[12vw]">
+    <div className="group bg-zinc-900 col-span relative">
       <img
         className="
-                cursor-pointer
-                object-cover
-                transition
-                duration
-                shadow-xl
-                rounded-md
-                group-hover:opacity-90
-                sm:group-hover:opacity-0
-                delay-300
-                w-full
-                h-[12vw]"
+          cursor-pointer
+          object-cover
+          transition
+          duration
+          shadow-xl
+          rounded-md
+          group-hover:opacity-90
+          delay-300
+          w-full
+          h-auto"
         src={data?.thumbnailUrl}
         alt="Thumbnail"
       />
